62d14b882a18e51ed680d6d768f97ab8e501c130
Colin Vogt <colin.vogt@divtec.ch>
2026-08-19 12:21:57 +0200

    Commencer le Jalon 2

--- src/YahtzeeProcedural.java
+++ src/YahtzeeProcedural.java
@@ -1,3 +1,4 @@
+import java.util.Scanner; // import the Scanner class
 public class YahtzeeProcedural {
 
     static final int NBRE_FACE = 6;
@@ -20,5 +21,24 @@ public class YahtzeeProcedural {
             deTire[i] = genereFaceAleatoire(NBRE_FACE);
         }
         afficherDee(deTire);
+
+        System.out.println("\nQuel dés voulez vous relancez ? (Saisir de 1-5 ou 0 si vous ne voulez pas relancer)\n");
+        for (int index = 0; index < deTire.length; index++) {
+            System.out.println("Dé " + (index + 1));
+        }
+
+        String desChoisi;
+        Scanner myObj = new Scanner(System.in);
+
+
+        do {
+            desChoisi = myObj.nextLine();
+
+            if (desChoisi == 0 || desChoisi.isEmpty())
+
+
+
+        } while ()
+
     }
 }
