cf2976b852b9831b5ded89f0c87eff154027562f
Colin Vogt <colin.vogt@divtec.ch>
2026-09-16 10:50:51 +0200

    Commencer la class Category

---
+++ src/Category.java
@@ -0,0 +1,14 @@
+public enum Category {
+    unePaire(),
+    deuxPaire(),
+    brelan(),
+    carre(),
+    fullHouse(),
+    petiteSuite(),
+    grandeSuite(),
+    yatzee()
+
+    private int nom;
+
+
+}
