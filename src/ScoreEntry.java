5a33b6c59355b7d7338953c184374de4d5b4a342
Colin Vogt <colin.vogt@divtec.ch>
2026-09-16 12:02:24 +0200

    Commencer la classe ScoreEntry

--- src/ScoreEntry.java
+++ src/ScoreEntry.java
@@ -1,2 +1,14 @@
 public class ScoreEntry {
+
+    private final Category category;
+    private final int NBRE_DE_MAX = 5;
+    private final int POINT_OBTENU = 0;
+    private final int[] JET_FINAL = new int[NBRE_DE_MAX];
+
+    ScoreEntry (Category category, int pointObtenu, int[] jetFinal) {
+        this.category = category;
+        this.POINT_OBTENU = pointObtenu;
+        this.JET_FINAL = jetFinal;
+    }
+
 }
