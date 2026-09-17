2e00fbca9d4f055c508b45378688aa74f1df0fea
Colin Vogt <colin.vogt@divtec.ch>
2026-09-17 15:19:51 +0200

    Fini la classe Player

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,18 @@
 public class Player {
+
+    private final String NOM;
+    private final Scorecard FEUILLE_SCORE;
+
+    Player(String nom, Scorecard feuilleScore) {
+        this.NOM = nom;
+        this.FEUILLE_SCORE = feuilleScore;
+    }
+
+    public String getNom() {
+        return NOM;
+    }
+
+    public Scorecard getFeuilleScore() {
+        return FEUILLE_SCORE;
+    }
 }
--- src/ScoreEntry.java
+++ src/ScoreEntry.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class ScoreEntry {
 
     private final Category CATEGORY;
