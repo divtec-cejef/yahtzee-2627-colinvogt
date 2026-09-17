01a5f8ddf652d458439f2d4a0ba45724999e22ce
Colin Vogt <colin.vogt@divtec.ch>
2026-09-17 14:37:12 +0200

    Fini la classe Scorecard

--- src/Scorecard.java
+++ src/Scorecard.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class Scorecard {
+    private ArrayList<ScoreEntry> historique =  new ArrayList<>();
+
+    public void ajouterScore(ScoreEntry entry) {
+        historique.add(entry);
+    }
+
+    public boolean estUtilisee(Category categorie) {
+        for (ScoreEntry entry : historique) {
+        if (entry.getCategory() == categorie) {
+            return true;
+        }}
+        return false;
+    }
+
+    public int calculPointTotal (){
+        int pointTotal = 0;
+        for (ScoreEntry entry : historique) {
+            pointTotal += entry.getPointObtenu();
+        }
+        return pointTotal;
+    }
+
 }
