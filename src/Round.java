24834f76b17b55e9f36e434eba7bbf9460c49d69
Colin Vogt <colin.vogt@divtec.ch>
2026-09-23 11:53:40 +0200

    feat : ajouter un constructeur a la class Round

--- src/Round.java
+++ src/Round.java
@@ -1,2 +1,9 @@
 public class Round {
+    private final ConsoleIO io;
+    private final DiceHand diceHand;
+
+    Round(ConsoleIO io) {
+        this.io = io;
+        this.diceHand = new DiceHand();
+    }
 }
