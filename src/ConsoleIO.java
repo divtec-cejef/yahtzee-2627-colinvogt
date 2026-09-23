72fd328efac0c5168bab76f8382be5cd67d8585a
Colin Vogt <colin.vogt@divtec.ch>
2026-09-23 10:00:03 +0200

    feat : Ajouter commentaire javadoc pour ConsoleIO

--- src/ConsoleIO.java
+++ src/ConsoleIO.java
@@ -1,16 +1,27 @@
 import java.util.Scanner;
 
 public class ConsoleIO{
-    private Scanner myObj;
+    private final Scanner myObj;
 
-    ConsoleIO (Scanner myObj) {
-        this.myObj = myObj;
+    /**
+     * Constructeur ConsoleIO
+     */
+    ConsoleIO() {
+        this.myObj = new Scanner(System.in);
     }
 
+    /**
+     * Affiche le prompte mis en parametre
+     * @param prompt
+     */
     public void afficher(String prompt) {
         System.out.println(prompt);
     }
 
+    /**
+     * Lit le prompte inserer par l'utilisateur
+     * @return
+     */
     public String lireLigne() {
         return myObj.nextLine();
     }
