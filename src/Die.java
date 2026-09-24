341d8d584d5dd74b9edcf2876ff4a476d9ef1a6a
Colin Vogt <colin.vogt@divtec.ch>
2026-09-24 14:15:57 +0200

    doc : Ajouter des commentaire javadoc pour chaque methode

--- src/Die.java
+++ src/Die.java
@@ -3,14 +3,25 @@ public class Die {
     private final int sideCount = 6;
     private int visibleSide;
 
+    /**
+     * Constructeur
+     */
     Die() {
     }
 
-public int roll() {
+    /**
+     * Lance les dés
+     * @return un chiffre aleatoire entre 1 et 6
+     */
+    public int roll() {
         this.visibleSide = (int) (Math.random() * sideCount + 1);
         return this.visibleSide;
     }
 
+    /**
+     * Obtient la face tiré
+     * @return la face tiré
+     */
     public int getVisibleSide() {
         return this.visibleSide;
     }
