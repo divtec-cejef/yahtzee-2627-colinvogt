6ed411aa09e37fb4ace943d73a2b5e489bfc36fe
Colin Vogt <colin.vogt@divtec.ch>
2026-09-24 14:06:46 +0200

    doc : Ajouter des commentaire javadoc pour chaque methode

--- src/Scorecard.java
+++ src/Scorecard.java
@@ -3,12 +3,21 @@ import java.util.ArrayList;
 public class Scorecard {
     private ArrayList<ScoreEntry> historique = new ArrayList<>();
 
+    /**
+     * Ajoute le Score a un liste de score
+     * @param entry le score a ajouter
+     */
     public void ajouterScore(ScoreEntry entry) {
         if (!estUtilisee(entry.getCategory())) {
             historique.add(entry);
         }
     }
 
+    /**
+     * Verifie si la categorie mise en parametre est utiliser
+     * @param categorie la categorie a verifier
+     * @return True si la category est utiliser, sinon false
+     */
     public boolean estUtilisee(Category categorie) {
         for (ScoreEntry entry : historique) {
             if (entry.getCategory() == categorie) {
@@ -18,6 +27,10 @@ public class Scorecard {
         return false;
     }
 
+    /**
+     * Calcule le nombre de point total
+     * @return le nombre de point total
+     */
     public int calculPointTotal() {
         int pointTotal = 0;
         for (ScoreEntry entry : historique) {
