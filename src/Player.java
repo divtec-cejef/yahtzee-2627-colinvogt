ff8769b5c9ff7964174c5f654f0f51ef58dd91ee
Colin Vogt <colin.vogt@divtec.ch>
2026-09-24 14:17:15 +0200

    doc : Ajouter des commentaire javadoc pour chaque methode

--- src/Player.java
+++ src/Player.java
@@ -3,15 +3,28 @@ public class Player {
     private final String NOM;
     private final Scorecard FEUILLE_SCORE;
 
+    /**
+     * Constuit le joueur
+     * @param nom nom du joueur
+     * @param feuilleScore feuille de score du joueur
+     */
     Player(String nom, Scorecard feuilleScore) {
         this.NOM = nom;
         this.FEUILLE_SCORE = feuilleScore;
     }
 
+    /**
+     * Obtient le nom du joueur
+     * @return le nom du joueur
+     */
     public String getNom() {
         return NOM;
     }
 
+    /**
+     * Obtient la feuille de score du joueur
+     * @return la feuille de score du joueur
+     */
     public Scorecard getFeuilleScore() {
         return FEUILLE_SCORE;
     }
